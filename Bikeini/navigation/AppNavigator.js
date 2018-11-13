6b2505e7d66e710951bd087aba6669021f533bf5
Jakob Andersson <jaan0006@student.uu.se>
2018-11-13 22:15:28 +0100

    Created first version of the browser page which only includes two components which makes up a flatlist of bicycle-items and some text-divs, also changed AppNavigator so that 'Browser.js' is the initialRoute

--- Bikeini/navigation/AppNavigator.js
+++ Bikeini/navigation/AppNavigator.js
@@ -2,14 +2,16 @@ import { createStackNavigator } from 'react-navigation';
 import Login from '../screens/Login';
 import TempPage from '../screens/TempPage';
 import SignUp from '../screens/SignUp';
+import Browser from '../screens/Browser';
 
 const AppNavigator = createStackNavigator({
   Login: { screen: Login },
   TempPage: { screen: TempPage },
   SignUp: { screen: SignUp },
+  Browser: { screen: Browser},
 },
 {
-  initialRouteName: 'Login',
+  initialRouteName: 'Browser',
 });
 
 export default AppNavigator;
---
+++ Bikeini/screens/Browser.js
@@ -0,0 +1,179 @@
+import React from 'react';
+import {
+  StyleSheet, Text, View, FlatList,
+} from 'react-native';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const stylesItem = StyleSheet.create({
+  item: {
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    width: '100%',
+    height: 100,
+    borderWidth: 0,
+    borderBottomWidth: 1,
+  },
+  image: {
+    alignSelf: 'center',
+    width: '20%',
+    height: '80%',
+    marginLeft: '3%',
+    backgroundColor: 'blue',
+  },
+  textView: {
+    alignSelf: 'center',
+    flexDirection: 'column',
+    marginBottom: '5%',
+    marginLeft: '5%',
+  },
+  description: {
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  location: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  commentsTag: {
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    width: 25,
+    height: 25,
+    right: '5%',
+    borderWidth: 1,
+    backgroundColor: 'white',
+  },
+  locationTag: {
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    width: 25,
+    height: 25,
+    right: '13%',
+    backgroundColor: 'black',
+  },
+});
+
+class Item extends React.PureComponent {
+  render() {
+    const { name, location } = this.props;
+    return (
+      <View style={stylesItem.item}>
+        <View style={stylesItem.image} />
+        <View style={stylesItem.textView}>
+          <Text style={stylesItem.description}>
+            {name}
+          </Text>
+          <Text style={stylesItem.location}>
+            {location}
+          </Text>
+        </View>
+        <View style={stylesItem.commentsTag} />
+        <View style={stylesItem.locationTag} />
+      </View>
+    );
+  }
+}
+
+Item.propTypes = {
+  name: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  header: {
+    alignSelf: 'flex-start',
+    marginTop: '5%',
+    marginLeft: '10%',
+  },
+  headerText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  filter: {
+    alignSelf: 'flex-start',
+    marginTop: '5%',
+    marginLeft: '10%',
+  },
+  browserList: {
+    flex: 1,
+    alignSelf: 'flex-start',
+    marginTop: '10%',
+    marginLeft: '10%',
+    width: '88%',
+  },
+});
+
+class Browser extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showMissing: true,
+      bicycles: [
+        { key: '1', name: 'Bicycle 1', location: 'Gränby' },
+        { key: '2', name: 'Bicycle 2', location: 'Gottsunda' },
+        { key: '3', name: 'Bicycle 3', location: 'Stenhagen' },
+        { key: '4', name: 'Bicycle 4', location: 'Gränby' },
+        { key: '5', name: 'Bicycle 5', location: 'Gottsunda' },
+        { key: '6', name: 'Bicycle 6', location: 'Stenhagen' }],
+    };
+  }
+
+  keyExtractor = item => item.key;
+
+  renderItem = ({ item }) => (
+    <Item name={item.name} location={item.location} />
+  );
+
+  render() {
+    const { showMissing, bicycles } = this.state;
+    let header;
+    if (showMissing) {
+      header = (
+        <View style={styles.header}>
+          <Text style={styles.headerText}>
+            Missing bikes in &#34;region&#34;
+          </Text>
+        </View>
+      );
+    } else {
+      header = (
+        <View style={styles.header}>
+          <Text style={styles.headerText}>
+            Found bikes in &#34;region&#34;
+          </Text>
+        </View>
+      );
+    }
+    return (
+      <View style={styles.container}>
+        {header}
+        <View style={styles.filter}>
+          <Text>Filter * </Text>
+        </View>
+        <View style={styles.browserList}>
+          <FlatList
+            data={bicycles}
+            extraData={this.state}
+            keyExtractor={this.keyExtractor}
+            renderItem={this.renderItem}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { loginState } = state;
+  return { loginState };
+};
+
+export default connect(mapStateToProps)(Browser);

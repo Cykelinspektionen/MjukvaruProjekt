93eb4d0506f9bc9bbae06beeec929952a00736c0
Jakob Andersson <jaan0006@student.uu.se>
2018-11-15 23:02:16 +0100

    Updated Browser-screen to use the components in the 'components' directory

--- Bikeini/screens/Browser.js
+++ Bikeini/screens/Browser.js
@@ -1,263 +1,10 @@
 import React from 'react';
 import {
-  StyleSheet, Text, View, FlatList, TouchableOpacity, ListView,
+  StyleSheet, Text, View, FlatList, TouchableOpacity,
 } from 'react-native';
-import { CheckBox } from 'react-native-elements';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-
-const stylesFilter = StyleSheet.create({
-  fullContainter: {
-    width: '100%',
-    height: '50%',
-    marginTop: '2%',
-  },
-  container: {
-    alignSelf: 'flex-start',
-    alignItems: 'flex-start',
-    width: '80%',
-    height: '100%',
-    marginLeft: '10%',
-    borderWidth: 1,
-  },
-  searchBar: {
-    height: '10%',
-    width: '70%',
-    borderWidth: 1,
-  },
-  breakLine: {
-    width: '100%',
-    height: '1%',
-    borderWidth: 0,
-    borderBottomWidth: 1,
-  },
-  boxPanel: {
-    flexDirection: 'row',
-    marginTop: '5%',
-    width: '100%'
-  },
-  rowContainer: {
-    flexDirection: 'row',
-    height: 30,
-    width: '100%',
-    backgroundColor: 'red',
-  },
-  itemContainer: {
-    flexDirection: 'row',
-    width: '50%',
-    height: '100%',
-    backgroundColor: 'white',
-  },
-  itemText: {
-    fontSize: 22,
-  }
-});
-
-
-const filterItems = [
-  '1', '2',
-  '3', '4',
-  '5', '6',
-];
-/*
-let checkBoxes = [
-  false, false,
-  false, false,
-];
-*/
-
-class ItemCheckbox extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isChecked: false,
-            title: this.props.title,
-            id: this.props.id,
-        }
-    }
-
-    changeStatus = () => {
-        const { id } = this.state;
-        this.setState({isChecked: !this.state.isChecked}, () => {
-            this.props.onChange(id);
-        });
-    }
-
-    render() {
-      const { title } = this.state;
-      return(
-          <CheckBox
-              title={this.state.title}
-              checkedIcon='dot-circle-o'
-              uncheckedIcon='circle-o'
-              checked={this.state.isChecked}
-              containerStyle={{backgroundColor: 'white', borderWidth: 0}}
-              onPress={this.changeStatus}
-           />
-      );
-    }
-}
-
-class Filter extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: ds.cloneWithRows(this.processFilterItems(filterItems)),
-    };
-
-    this.updateCheckBoxes = this.updateCheckBoxes.bind(this);
-    this.checkBoxes = [];
-  }
-
-  updateCheckBoxes(id) {
-    let newCheckBoxes = this.checkBoxes;
-    let ind = id%2;
-    let rowInd = (ind-id)/2;
-    newCheckBoxes[rowInd][ind] = !newCheckBoxes[rowInd][ind];
-
-    this.checkBoxes = newCheckBoxes;
-    console.log(this.checkBoxes);
-  }
-
-  processFilterItems(filterItems) {
-    let row = [];
-    let processedFilter = [];
-    let newCheckBoxes = [];
-    for(let i=0; i<filterItems.length; i=i+2) {
-        row.push(i);
-        row.push(i+1);
-
-        row.push(filterItems[i]);
-        row.push(filterItems[i+1]);
-
-        newCheckBoxes.push([false, false]);
-
-        //newCheckBoxes.push(false);
-
-        //processedFilter.push(filterItems[i]);
-        processedFilter.push(row);
-        row = [];
-    }
-    console.log(processedFilter);
-    console.log(newCheckBoxes);
-    this.checkBoxes = newCheckBoxes;
-    return processedFilter;
-  }
-
-  renderRow(rowData) {
-    //return <ItemCheckbox />;
-
-    let id1 = rowData[0];
-    let text1 = rowData[2];
-    let id2 = rowData[1];
-    let text2 = rowData[3];
-    return (
-      <View style={stylesFilter.rowContainer}>
-        <View style={stylesFilter.itemContainer}>
-          <ItemCheckbox title={text1} id={id1} onChange={this.updateCheckBoxes}/>
-        </View>
-        <View style={stylesFilter.itemContainer}>
-          <ItemCheckbox title={text2} id={id2} onChange={this.updateCheckBoxes}/>
-        </View>
-      </View>
-    );
-  }
-
-  render() {
-    return (
-      <View style={stylesFilter.fullContainter}>
-        <View style={stylesFilter.container}>
-          <View style={stylesFilter.searchBar} />
-          <ListView
-            dataSource={this.state.dataSource}
-            renderRow={(rowData) => this.renderRow(rowData)}
-          />
-        </View>
-        <View style={stylesFilter.breakLine} />
-      </View>
-    );
-  }
-}
-
-const stylesItem = StyleSheet.create({
-  item: {
-    alignItems: 'flex-start',
-    flexDirection: 'row',
-    width: '100%',
-    height: 100,
-    borderWidth: 0,
-    borderBottomWidth: 1,
-  },
-  image: {
-    alignSelf: 'center',
-    width: '20%',
-    height: '80%',
-    marginLeft: '3%',
-    backgroundColor: 'blue',
-  },
-  textView: {
-    alignSelf: 'center',
-    flexDirection: 'column',
-    marginBottom: '5%',
-    marginLeft: '5%',
-  },
-  description: {
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  location: {
-    fontSize: 16,
-    fontWeight: '500',
-  },
-  commentsTag: {
-    position: 'absolute',
-    alignSelf: 'flex-end',
-    width: 25,
-    height: 25,
-    right: '5%',
-    borderWidth: 1,
-    backgroundColor: 'white',
-  },
-  locationTag: {
-    position: 'absolute',
-    alignSelf: 'flex-end',
-    width: 25,
-    height: 25,
-    right: '13%',
-    backgroundColor: 'black',
-  },
-});
-
-class Item extends React.Component {
-  render() {
-    const { name, location } = this.props;
-    return (
-      <View style={stylesItem.item}>
-        <View style={stylesItem.image} />
-        <View style={stylesItem.textView}>
-          <Text style={stylesItem.description}>
-            {name}
-          </Text>
-          <Text style={stylesItem.location}>
-            {location}
-          </Text>
-        </View>
-        <View style={stylesItem.commentsTag} />
-        <View style={stylesItem.locationTag} />
-      </View>
-    );
-  }
-}
-
-Item.propTypes = {
-  name: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-};
+import Filter from '../components/Filter';
+import Item from '../components/Item';
 
 const styles = StyleSheet.create({
   container: {
@@ -369,16 +116,21 @@ class Browser extends React.Component {
   renderFilter = () => {
     const { showFilter } = this.state;
 
-    if(showFilter) {
+    if (showFilter) {
       return <Filter />;
     }
-    else {
-      return;
-    }
+
+    return null;
+  }
+
+  changeFilterStatus = () => {
+    const { showFilter } = this.state;
+
+    this.setState({ showFilter: !showFilter });
   }
 
   render() {
-    const { bicycles, showFilter } = this.state;
+    const { bicycles } = this.state;
     const header = this.renderHeader();
     const filter = this.renderFilter();
     return (
@@ -388,9 +140,9 @@ class Browser extends React.Component {
           style={styles.showType}
           onPress={this.switchPageType}
         />
-        <TouchableOpacity 
+        <TouchableOpacity
           style={styles.filter}
-          onPress={() => this.setState({showFilter: !this.state.showFilter})}
+          onPress={this.changeFilterStatus}
         >
           <Text>Filter * </Text>
         </TouchableOpacity>

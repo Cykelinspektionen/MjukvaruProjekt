9e215b68582a6b9f9f369731de521af9b6b048b6
erikajansson1 <erika_jansson1@hotmail.com>
2018-11-22 14:08:16 +0100

    Header font now support both iOS and Android

--- Bikeini/App.js
+++ Bikeini/App.js
@@ -2,14 +2,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Font } from 'expo';
 import thunk from 'redux-thunk';
 import reducers from './navigation/reducers/index';
 import AppNavigator from './navigation/AppNavigator';
 
 
 const store = createStore(reducers, applyMiddleware(thunk));
+const headerFont = require('./assets/Noteworthy-Bold.ttf');
 
 export default class App extends React.Component {
+  componentDidMount() {
+    Font.loadAsync({
+      CustomFont: headerFont,
+    });
+  }
+
   render() {
     return (
       <Provider store={store}>

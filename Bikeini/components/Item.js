d9567c980077fd0eddae3fa70aa5a32b13f2eb45
Fredrik Söderberg <soderbergh@gmail.com>
2018-11-28 15:11:24 +0100

    fixed css rules and touchability on buttons in Item

--- Bikeini/components/Item.js
+++ Bikeini/components/Item.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  StyleSheet, Text, View, Image,
+  StyleSheet, Text, View, Image, TouchableOpacity, Alert,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
@@ -29,6 +29,8 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     marginBottom: '5%',
     marginLeft: '5%',
+    flexWrap: 'wrap',
+    flex: 1,
   },
   description: {
     fontSize: 18,
@@ -39,20 +41,20 @@ const styles = StyleSheet.create({
     fontWeight: '500',
   },
   commentsTag: {
-    position: 'absolute',
-    alignSelf: 'flex-end',
     width: 25,
     height: 25,
-    right: '5%',
-    marginBottom: 10,
+    marginBottom: 5,
   },
   locationTag: {
-    position: 'absolute',
-    alignSelf: 'flex-end',
     width: 25,
     height: 25,
-    right: '13%',
-    marginBottom: 10,
+    marginBottom: 5,
+  },
+  buttonRow: {
+    flex: 0.25,
+    flexDirection: 'row-reverse',
+    alignSelf: 'flex-end',
+    alignContent: 'flex-end',
   },
 });
 
@@ -71,8 +73,21 @@ export default class Item extends React.PureComponent {
             {model}
           </Text>
         </View>
-        <Image style={styles.commentsTag} source={commentIcon} />
-        <Image style={styles.locationTag} source={locationIcon} />
+        <View style={styles.buttonRow}>
+          <TouchableOpacity
+            style={styles.commentsTag}
+            onPress={() => Alert.alert('Do something!')}
+          >
+            <Image style={styles.commentsTag} source={commentIcon} />
+
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.locationTag}
+            onPress={() => Alert.alert('Do something!')}
+          >
+            <Image style={styles.locationTag} source={locationIcon} />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
